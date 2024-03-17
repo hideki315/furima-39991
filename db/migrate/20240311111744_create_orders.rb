d396ed52e3e03d2fca782cf3f0c4eecfadba5569
hideki315 <shb1228.8.8@gmail.com>
2024-03-17 18:53:26 +0900

    デプロイ

--- db/migrate/20240311111744_create_orders.rb
+++ db/migrate/20240311111744_create_orders.rb
@@ -1,10 +1,10 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
-    create_table :orders do |t|
-      t.references :item, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
-
-      t.timestamps
+    unless table_exists?(:orders)
+      create_table :orders do |t|
+        t.references :item, null: false, foreign_key: true
+        t.references :user, null: false, foreign_key: true
+      end
     end
   end
 end
